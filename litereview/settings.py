a200b23beb0091dec28b549b1ad10b81a7da6fe8
Valkea <github-sub@shedge.com>
2021-01-30 16:59:12 +0100

    feat: add 'flux' app and define models
    refactor: add Pillow to the requirements.txt so we wan use ImageField

--- litereview/settings.py
+++ litereview/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flux',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Define the default AUTH_USER_MODEL value
+AUTH_USER_MODEL = 'flux.User'
