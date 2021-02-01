14741b9c7356bc016ecc18ee6a413748a6831641
Valkea <github-sub@shedge.com>
2021-02-01 01:34:55 +0100

    feat: hide 'Currently: xxx' in the image field when editing a ticket

--- apps/reviews/forms.py
+++ apps/reviews/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm  # , HiddenInput
+from django.forms import ModelForm, ImageField, FileInput  # , HiddenInput
 
 from apps.reviews.models import Ticket, Review
 
 
 class TicketForm(ModelForm):
+
+    image = ImageField(
+            label=('Image'),
+            required=False,
+            error_messages={'invalid': ("Image files only")},
+            widget=FileInput,
+            )
+
     class Meta:
         model = Ticket
         fields = ["title", "description", "image", "user"]
