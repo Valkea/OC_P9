1cc1a8398ea8812aa9e0e9ef90bc0f2407c985a4
Valkea <github-sub@shedge.com>
2021-02-08 17:58:40 +0100

    refactor: now display ratings as radioselect with range 0 to 5

--- apps/reviews/forms.py
+++ apps/reviews/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ImageField, FileInput  # , HiddenInput
+from django.forms import ModelForm, ImageField, FileInput, RadioSelect, ChoiceField
 
 from apps.reviews.models import Ticket, Review
 
@@ -23,6 +23,11 @@ class TicketForm(ModelForm):
 
 
 class ReviewForm(ModelForm):
+    # rating = CharField(label='Rating', widget=TextInput(attrs={'min': 1, 'max': '5', 'type': 'number'}))
+    CHOICES = [(str(x), str(x)) for x in range(0, 6)]
+    # CHOICES = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
+    rating = ChoiceField(label='Evaluation', widget=RadioSelect, choices=CHOICES)
+
     class Meta:
         model = Review
         fields = ["headline", "rating", "body"]
